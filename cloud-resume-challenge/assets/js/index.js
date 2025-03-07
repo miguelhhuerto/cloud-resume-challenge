c1175c5af11389528d900aea979240dfd313ce34
miguelhhuerto <miguelhhuerto@gmail.com>
2025-03-07 19:32:28 +0800

    Fix Link for API Gateway

--- cloud-resume-challenge/assets/js/index.js
+++ cloud-resume-challenge/assets/js/index.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
-    let response = await fetch('https://6lahwmgs54.execute-api.ap-southeast-2.amazonaws.com/dev/counter');
+    let response = await fetch('https://6lahwmgs54.execute-api.ap-southeast-2.amazonaws.com/dev/resume-function-final');
     let data = await response.json();
     counter.innerHTML = `Website Visits: ${data.views}`;
 }
