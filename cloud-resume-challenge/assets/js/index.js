e0ce71d6f9979d11cf66b094fee6a82a49c4ab28
miguelhhuerto <miguelhhuerto@gmail.com>
2025-03-07 17:58:49 +0800

    Fix Counter Text

--- cloud-resume-challenge/assets/js/index.js
+++ cloud-resume-challenge/assets/js/index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch('https://kbat9glkqb.execute-api.us-east-1.amazonaws.com/dev/cloud-resume-test-api');
     let data = await response.json();
-    counter.innerHTML = `Page Visits: ${data.views}`;
+    counter.innerHTML = `Website Visits: ${data.views}`;
 }
 
 updateCounter();
